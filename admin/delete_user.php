7e554e1bb4ab3af0bc3ba498cbfdde05f68454cc
Junayed Khan <junayed03aiub@gmail.com>
2026-08-30 01:23:26 +0600

    add delete_user code2

--- admin/delete_user.php
+++ admin/delete_user.php
@@ -7,5 +7,16 @@ if (!isset($_SESSION['user_id']) || $_SESSION['user_role'] != 'admin') {
     exit();
 }
 
+if (isset($_GET['id'])) {
+    $user_id = $_GET['id'];
 
+    $sql = "DELETE FROM users WHERE id='$user_id'";
+
+    if (mysqli_query($conn, $sql)) {
+        header("Location: admin_dashboard.php");
+        exit();
+    } else {
+        echo "Error deleting record: " . mysqli_error($conn);
+    }
+}
 ?>
