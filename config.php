1100abd3ba347f1898206271e7df39d5f85bb294
samiaalam_01 <samiaalam4484.usa2@gmail.com>
2026-08-30 11:56:59 +0600

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "webtech_project";
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection Failed: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8mb4");
+
+?>
