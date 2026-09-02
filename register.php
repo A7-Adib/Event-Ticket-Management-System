66849c456a5261ffd0c76635326a794de3f3365f
Junayed Khan <junayed03aiub@gmail.com>
2026-09-02 13:58:12 +0600

    add password

--- register.php
+++ register.php
@@ -11,7 +11,7 @@ if (isset($_POST['register'])) {
 
     if ($password != $confirm_password) {
 
-        echo "Passwords do not match!";
+        echo "Passwords do not match!..";
 
     } else {
 
